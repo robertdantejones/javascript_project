ea8eae6e5ee82791a5c44deacc2e3c239d17b85c
Robert Jones <robertdante@outlook.com>
2023-08-10 22:50:22 -0400

    task list

---
+++ project.js
@@ -0,0 +1,8 @@
+// 1. Deposit some money
+// 2. determine number of lines to bet on
+// 2. Collect a bet amount
+// 3. collect a bet amount 
+// 4. spin the slot machine
+// 5. check if the user won
+// 6. give the user their winnings or take bet if lost.
+// 7. play again
