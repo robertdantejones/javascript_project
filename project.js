cfd13f47851a136bf204ced349bb746d1d704bf1
Robert Jones <robertdante@outlook.com>
2023-08-13 13:03:00 -0400

    add number of lines, and bet functions

--- project.js
+++ project.js
@@ -22,9 +22,52 @@ const deposit = () => {
     // check validity in number
     if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
       console.log("Invalid deposit amount, please try again.");
+    //returns amount if user enters a valid input
+    } else {
+        return numberDepositAmount;
     }
   }
 };
 
-deposit();
+const getNumberOfLines = () => {
+  // making the user enter the number of lines again if it's invalid.
+  while (true) {
+    const lines = prompt("Enter the number of lines to bet on (1-3): ");
+    // convert line amount to integer
+    // note to self: "numberOfLines" made as a variable bc you would want to add to it or subtract from it.
+    const numberOfLines = parseFloat(lines);
+
+    // check validity in number
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+      console.log("Invalid number of lines, please try again.");
+      //returns amount if user enters a valid input
+    } else {
+      return numberOfLines;
+    }
+  }
+};
+
+const getBet = (balance) => {
+  // making the user enter the bet again if it's invalid.
+  while (true) {
+    const bet = prompt("Enter the total bet: ");
+    // convert bet amount amount to integer
+    // note to self: "numberBet" made as a variable bc you would want to add to it or subtract from it.
+    const numberBet = parseFloat(bet);
+
+    // check validity in number
+    if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance) {
+      console.log("Bet amount is invalid, please try again.");
+      //returns amount if user enters a valid input
+    } else {
+      return numberBet;
+    }
+  }
+};
+
+// note to self: allowed for the value to be changed. 
+let balance = deposit();
+const numberOfLines = getNumberOfLines();
+const bet = getBet(balance);
+
 
